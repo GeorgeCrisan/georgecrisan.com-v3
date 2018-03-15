9c2cc27ffb26704e701c0b45047c8d9bef63f9b5
GeorgeCrisan <georgerdp@gmail.com>
2018-03-15 21:46:55 +0000

    start

--- assets/bundle/bundle.js
+++ assets/bundle/bundle.js
@@ -1,4 +1,6 @@
+
 //@prepros-append jquery-3.3.1.min.js
+//@prepros-append popper.min.js
 //@prepros-append bootstrap.min.js
 //@prepros-append fontawesome-all.min.js
 //@prepros-append lodash.min.js
--- assets/bundle/mainjs.js
+++ assets/bundle/mainjs.js
@@ -1,3 +1,4 @@
 console.log(_);
 console.log($);
+
 console.log('merge');
