81c7f21f1c9be5597fab70e9b1c386d15f1afdb1
GeorgeCrisan <georgerdp@gmail.com>
2018-03-15 21:11:45 +0000

    first commit

---
+++ assets/bundle/bundle.js
@@ -0,0 +1,5 @@
+//@prepros-append jquery-3.3.1.min.js
+//@prepros-append bootstrap.min.js
+//@prepros-append fontawesome-all.min.js
+//@prepros-append lodash.min.js
+//@prepros-append mainjs.js
---
+++ assets/bundle/mainjs.js
@@ -0,0 +1,3 @@
+console.log(_);
+console.log($);
+console.log('merge');
